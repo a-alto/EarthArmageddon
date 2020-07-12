35e59d91da9334232c0d87987d92ea5d34c1b83a
Andrea Altomare <34966653+a-alto@users.noreply.github.com>
2020-07-12 12:38:58 +0200

    EarthArmageddon codebase
    EarthArmageddon codebase

---
+++ Codebase/EarthArmageddon/bin/Debug/FontInstaller.bat
@@ -0,0 +1,12 @@
+@echo off
+cls
+mkdir C:\EarthArmageddonFonts
+copy /Y bgothm.ttf C:\EarthArmageddonFonts
+copy /Y ANDROIDROBOT.ttf C:\EarthArmageddonFonts
+copy /Y CODEb.otf C:\EarthArmageddonFonts
+copy /Y CODEl.otf C:\EarthArmageddonFonts
+copy /Y tt0586m_.ttf C:\EarthArmageddonFonts
+copy /Y Xolonium-Bold.otf C:\EarthArmageddonFonts
+copy /Y Xolonium-Regular.otf C:\EarthArmageddonFonts
+copy /Y FREEDOM_0.ttf C:\EarthArmageddonFonts
+copy /Y MODERNESANS.ttf C:\EarthArmageddonFonts
