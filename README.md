ea26bf907c61b063e3ddbd28f16a6478e2d33b15
Andrea Altomare <34966653+a-alto@users.noreply.github.com>
2020-07-12 12:24:19 +0200

    EarthArmageddon game
    EarthArmageddon game

---
+++ README.md
@@ -0,0 +1,9 @@
+# Earth Armageddon
+
+This is a project I made in high school (year: 2015).  
+It's a 2D platform game based on a sci-fi scenario.
+
+Earth Armageddon is a _shoot 'em up_ (fixed shooter) in which the player goes through different levels by shooting up 
+enemy alien spaceships.
+
+More information can be found into `Relazione tecnica Earth Armageddon.docx` file.
---
+++ fonts/FontInstaller.bat
@@ -0,0 +1,13 @@
+@echo off
+cls
+mkdir C:\EarthArmageddonFonts
+copy /Y bgothm.ttf C:\EarthArmageddonFonts
+copy /Y bgothl.ttf C:\EarthArmageddonFonts
+copy /Y ANDROIDROBOT.ttf C:\EarthArmageddonFonts
+copy /Y CODEb.ttf C:\EarthArmageddonFonts
+copy /Y CODEl.ttf C:\EarthArmageddonFonts
+copy /Y tt0586m_.ttf C:\EarthArmageddonFonts
+copy /Y Xolonium-Bold.ttf C:\EarthArmageddonFonts
+copy /Y Xolonium-Regular.ttf C:\EarthArmageddonFonts
+copy /Y FREEDOM_0.ttf C:\EarthArmageddonFonts
+copy /Y MODERNESANS.ttf C:\EarthArmageddonFonts
